d1f325bb2c5d3886e20ad543efb502dff123c4ee
sarahchristine913 <s.mele13@gmail.com>
2014-07-16 10:02:44 -0400

    Mele_S Completed Assessment
    Q5 Incomplete

--- basics.js
+++ basics.js
@@ -4,19 +4,32 @@
 
 var pets = ['cat', 'dog', 'snake'];
 
+function printPets(element, array) {
+  console.log("I have a " + element);
+}
+
+pets.forEach(printPets);
+
 ///// Question 2
 // Change my friend's hair color to "green", without modifying the line below.
 
 var friend = { name: 'Chris', age: 27, hair_color: 'blue' };
+
+friend.replace
 
 ///// Question 3
 // The function below is problematic. Write code that demonstrates the problem,
 // then modify the function (preserving its purpose) to remove the problem.
 
 var square_plus_one = function(n){
-  squared = n * n;
+  var squared = n * n;
   return squared + 1;
 };
+
+// In the console, squared is available outside of the function, and remains
+// n*n. Eg. square_plus_one(5) sets squared to 25 and can be checked
+// outside of the function. **Squared here is being set as a global variable
+// as var was not declared. Added var to squared declaration/assignment
 
 ///// Question 4
 // Translate the Ruby method defined below into a JavaScript function.
@@ -31,6 +44,18 @@ def greet(name, excited = false)
   end
 end
 */
+var greeting = function(greeting, name, mood) {
+  mood = mood || bored;
+  greeting = greeting || 'Hi';
+  name = name || 'nobody';
+  if (mood === 'excited') {
+    console.log((greeting + ' ' + name + ' here!').toUpperCase);
+  }
+  else {
+    console.log(greeting + ' ' + name + ' here!');
+  }
+};
+
 
 ///// Question 5
 // Write a function called `twice` that accepts both a value and another
@@ -41,3 +66,10 @@ var phrase = 'Odelay!';
 var shoutIt = function(text){
   console.log(text.toUpperCase());
 };
+
+var twice = function(text, function){
+  console.log(function(text));
+
+}
+
+//Incomplete, know I want to create a closure expression here!
