2dfc5b3a7e642ab8b9176ce5d3767c5e31463a53
nnovak13 <nnovak@fordham.edu>
2014-07-16 10:00:24 -0400

    finsished assignment

--- basics.js
+++ basics.js
@@ -3,20 +3,30 @@
 // prefixed with the text "I have a " (so "I have a cat", "I have a dog", etc.)
 
 var pets = ['cat', 'dog', 'snake'];
+function allPets(pets) {
+  pets.forEach(function(pet) {
+    pets = pets.split(pet).join('');
+  });
+   console.log("I have a " + pets);
+}
 
 ///// Question 2
 // Change my friend's hair color to "green", without modifying the line below.
 
 var friend = { name: 'Chris', age: 27, hair_color: 'blue' };
 
+friend.hair_color = 'green';
+
 ///// Question 3
 // The function below is problematic. Write code that demonstrates the problem,
 // then modify the function (preserving its purpose) to remove the problem.
 
 var square_plus_one = function(n){
-  squared = n * n;
+  var squared = n * n;
   return squared + 1;
 };
+
+// Nick's comment: otherwise, without labeling squared as VAR, you have squared a global variable
 
 ///// Question 4
 // Translate the Ruby method defined below into a JavaScript function.
@@ -32,6 +42,20 @@ def greet(name, excited = false)
 end
 */
 
+var greet = function(name, excited) {
+  excited = excited || false;
+  name = name || " ";
+  var greeting = "Hi," + name + " here!";
+
+   if(excited !== false) {
+    greeting.toUpperCase();
+  }
+  return greeting;
+};
+
+
+
+
 ///// Question 5
 // Write a function called `twice` that accepts both a value and another
 // function as arguments, and calls the provided function twice, both times
@@ -41,3 +65,23 @@ var phrase = 'Odelay!';
 var shoutIt = function(text){
   console.log(text.toUpperCase());
 };
+
+
+
+var twice = function(phrase, shoutIt) {
+   twice.forEach(phrase, function(shoutIt) {
+
+  });
+   console.log((phrase)*2);
+
+};
+
+
+
+
+
+
+
+
+
+
