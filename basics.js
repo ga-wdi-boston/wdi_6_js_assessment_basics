742646c6288d6d6d164d4e4a244874d33a08153b
cyurtbil <cenky92@gmail.com>
2014-10-28 09:18:46 -0400

    Quiz finished

--- basics.js
+++ basics.js
@@ -4,10 +4,16 @@
 
 var pets = ['cat', 'dog', 'wombat'];
 
+for (var i = 0, length = pets.length; i < length; i++) {
+  console.log("I have a " + pets[i]);
+}
+
 ///// Question 2
 // Change my friend's hair color to "green", without modifying the line below.
 
 var friend = { name: 'Chris', age: 27, hairColor: 'blue' };
+
+friend.hairColor = "green"; 
 
 ///// Question 3
 // The function below is problematic. In 1-2 sentences explain why, and then fix the code.
@@ -16,6 +22,15 @@ var squarePlusOne = function(n){
   squared = n * n;
   return squared + 1;
 };
+
+// In decleration of variables in javascript we use 'var' keyword. If there is no var keyword then there 
+// could be unexpected results when we call the function
+
+var squarePlusOne = function(n){
+  var squared = n * n;
+  return squared + 1;
+};
+
 
 ///// Question 4
 // Translate the Ruby method defined below into a JavaScript function.
@@ -31,6 +46,15 @@ def greet(name, excited)
 end
 */
 
+var greet = function(name, excited) {
+  var greeting = "Hi, " + name + " here!";
+  if (excited === true) {
+    return greeting.upperCase();
+  } else {
+    return greeting;
+  }
+}
+
 ///// Question 5
 // Consider the following code.
 
@@ -42,15 +66,16 @@ var anna = {name: "Anna",
           }
 
 // What is `anna`? (e.g. method, function, string, etc.) 
-
+anna is an object.
 
 // What are `name`, `age`, and `luckyNumber`? 
-
+they are properties of object anna.
 
 // What does `this` refer to? 
-
+this refers the current object, which is anna.
 
 // If you are in console.log and you want to see anna's lucky number, how would you do that? 
-
+anna.luckyNumber();
 
 // If you want to change anna's name to wurble, how would you do that? 
+anna.name = 'wurble';
