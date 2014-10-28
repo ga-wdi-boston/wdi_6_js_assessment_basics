6485c362e2265c7eea2504ff4555505e545f307c
mattmcdevitt93 <mattmcdevitt93@gmail.com>
2014-10-28 09:30:43 -0400

    Javascript quiz

--- basics.js
+++ basics.js
@@ -3,11 +3,17 @@
 // prefixed with the text "I have a " (so "I have a cat", "I have a dog", etc.)
 
 var pets = ['cat', 'dog', 'wombat'];
+var loop = pets.length();
+
+for (l = 0; l < pets.length; l++) {
+console.log("I have a " + pet[l])
+};
 
 ///// Question 2
 // Change my friend's hair color to "green", without modifying the line below.
 
 var friend = { name: 'Chris', age: 27, hairColor: 'blue' };
+friends[2] = "green"
 
 ///// Question 3
 // The function below is problematic. In 1-2 sentences explain why, and then fix the code.
@@ -41,16 +47,22 @@ var anna = {name: "Anna",
             }
           }
 
-// What is `anna`? (e.g. method, function, string, etc.) 
+// What is `anna`? (e.g. method, function, string, etc.)
 
+// string
 
-// What are `name`, `age`, and `luckyNumber`? 
+// What are `name`, `age`, and `luckyNumber`?
 
+// this is a hash
 
-// What does `this` refer to? 
+// What does `this` refer to?
 
+// reffrences the parent object.
 
-// If you are in console.log and you want to see anna's lucky number, how would you do that? 
+// If you are in console.log and you want to see anna's lucky number, how would you do that?
 
+// anna.luckyNumber()
 
-// If you want to change anna's name to wurble, how would you do that? 
+// If you want to change anna's name to wurble, how would you do that?
+
+anna[0] = "wurble"
