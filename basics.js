0447025a9385ad943f15cace7cef3c64b58d204e
Daniel Tyson <danielctyson@gmail.com>
2015-02-17 09:34:45 -0500

    finished practice quiz

--- basics.js
+++ basics.js
@@ -3,17 +3,24 @@
 // prefixed with the text "I have a " (so "I have a cat", "I have a dog", etc.)
 
 var pets = ['cat', 'dog', 'wombat'];
+for (i in pets) {
+  console.log("I have a " + pets[i]);
+};
+
 
 ///// Question 2
 // Change my friend's hair color to "green", without modifying the line below.
 
 var friend = { name: 'Chris', age: 27, hairColor: 'blue' };
+friend.hairColor = 'green';
 
 ///// Question 3
 // The function below is problematic. In 1-2 sentences explain why, and then fix the code.
 
+// The variable squared is not declared so it has global scope.
+
 var squarePlusOne = function(n){
-  squared = n * n;
+  var squared = n * n;
   return squared + 1;
 };
 
@@ -31,6 +38,14 @@ def greet(name, excited)
 end
 */
 
+var greet = function(name, excited) {
+  var greeting = "Hi, " + name + " here!";
+  if(excited === true) {
+    return greeting.toUpperCase();
+  }
+  return greeting;
+};
+
 ///// Question 5
 // Consider the following code.
 
@@ -41,16 +56,20 @@ var anna = {name: "Anna",
             }
           }
 
-// What is `anna`? (e.g. method, function, string, etc.) 
+// What is `anna`? (e.g. method, function, string, etc.)
+// object
+
+// What are `name`, `age`, and `luckyNumber`?
+// properties
 
 
-// What are `name`, `age`, and `luckyNumber`? 
+// What does `this` refer to?
+//a pointer to the current object (anna)
 
 
-// What does `this` refer to? 
+// If you want to return the value of anna's lucky number, how would you do that?
+//anna.luckyNumber();
 
 
-// If you want to return the value of anna's lucky number, how would you do that? 
-
-
-// If you want to change anna's name to "wurble", how would you do that? 
+// If you want to change anna's name to "wurble", how would you do that?
+//anna.name = "wurble"
