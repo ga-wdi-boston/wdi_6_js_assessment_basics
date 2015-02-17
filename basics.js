5359436bb6a42961ab26686b285aae082902f435
Ashlinn Brennan <asbren13@gmail.com>
2015-02-17 09:42:13 -0500

    practice quiz complete

--- basics.js
+++ basics.js
@@ -4,18 +4,27 @@
 
 var pets = ['cat', 'dog', 'wombat'];
 
+for(pet in pets){
+  console.log("I have a " + pets[pet]);
+};
+
 ///// Question 2
 // Change my friend's hair color to "green", without modifying the line below.
 
 var friend = { name: 'Chris', age: 27, hairColor: 'blue' };
 
+friend.hairColor = 'green';
+console.log(friend.hairColor)
 ///// Question 3
 // The function below is problematic. In 1-2 sentences explain why, and then fix the code.
 
 var squarePlusOne = function(n){
-  squared = n * n;
+  var squared = n * n;
   return squared + 1;
 };
+
+console.log(squarePlusOne(2));
+// This is problematic because the variable is not declared
 
 ///// Question 4
 // Translate the Ruby method defined below into a JavaScript function.
@@ -31,6 +40,15 @@ def greet(name, excited)
 end
 */
 
+var greet = function(name, excited){
+  var greeting = "Hi " + name + " here";
+    if(excited === true){
+      return greeting.upcase;
+    };
+    return greeting;
+};
+
+// console.log(greet());
 ///// Question 5
 // Consider the following code.
 
@@ -41,16 +59,18 @@ var anna = {name: "Anna",
             }
           }
 
-// What is `anna`? (e.g. method, function, string, etc.) 
+// What is `anna`? (e.g. method, function, string, etc.)
+// string
 
+// What are `name`, `age`, and `luckyNumber`?
+// objects
 
-// What are `name`, `age`, and `luckyNumber`? 
+// What does `this` refer to?
+// in this instance, this refers to var anna's age
 
+// If you want to return the value of anna's lucky number, how would you do that?
+console.log(anna.luckyNumber(2));
 
-// What does `this` refer to? 
-
-
-// If you want to return the value of anna's lucky number, how would you do that? 
-
-
-// If you want to change anna's name to "wurble", how would you do that? 
+// If you want to change anna's name to "wurble", how would you do that?
+anna.name = "wurble";
+console.log(anna.name);
