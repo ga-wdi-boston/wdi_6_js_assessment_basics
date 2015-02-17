b41f55d77be386a30045e19b5a6b92f30d4371fb
Charles Talbot <charlietalbot@gmail.com>
2015-02-17 09:51:43 -0500

    trying to commit basic.js

--- basics.js
+++ basics.js
@@ -3,33 +3,42 @@
 // prefixed with the text "I have a " (so "I have a cat", "I have a dog", etc.)
 
 var pets = ['cat', 'dog', 'wombat'];
+function animalArray(){
+  for (var i=0; i < pets.length; i++){
+    console.log("I have a " + pets[i]);
+  }
+};
+animalArray();
 
 ///// Question 2
 // Change my friend's hair color to "green", without modifying the line below.
 
 var friend = { name: 'Chris', age: 27, hairColor: 'blue' };
+friend.haircolor = null;
+console.log(friend);
 
 ///// Question 3
 // The function below is problematic. In 1-2 sentences explain why, and then fix the code.
 
-var squarePlusOne = function(n){
-  squared = n * n;
-  return squared + 1;
+//  'squared' is being built as a global variable, which is bad practice
+
+function squarePlusOne(n){
+  var f = (n * n) + 1;
+  return f;
 };
 
 ///// Question 4
 // Translate the Ruby method defined below into a JavaScript function.
 
-/*
-def greet(name, excited)
-  greeting = "Hi, #{name} here!"
-  if excited == true
-    greeting.upcase
-  else
-    greeting
-  end
-end
-*/
+
+function greet(name, excited){
+  var greeting = "Hi, " + name + " here!"
+  if excited === true {
+      greeting.uppercase;
+  } else {
+    greeting;
+  }
+};
 
 ///// Question 5
 // Consider the following code.
@@ -41,16 +50,22 @@ var anna = {name: "Anna",
             }
           }
 
-// What is `anna`? (e.g. method, function, string, etc.) 
+// What is `anna`? (e.g. method, function, string, etc.)
 
+//an object
 
-// What are `name`, `age`, and `luckyNumber`? 
+// What are `name`, `age`, and `luckyNumber`?
 
+//object properties
 
-// What does `this` refer to? 
+// What does `this` refer to?
 
+// like self in ruby
 
-// If you want to return the value of anna's lucky number, how would you do that? 
+// If you want to return the value of anna's lucky number, how would you do that?
 
+//anna.luckynumber();
 
-// If you want to change anna's name to "wurble", how would you do that? 
+// If you want to change anna's name to "wurble", how would you do that?
+
+//anna.name......something
