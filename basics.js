06bd6051295582f5556df886eb41e4d5dc15f13f
J.Marx <jennyrutledge@gmail.com>
2015-02-17 09:40:50 -0500

    Complete pre-quiz

--- basics.js
+++ basics.js
@@ -4,18 +4,26 @@
 
 var pets = ['cat', 'dog', 'wombat'];
 
+for(i in pets){console.log("I have a " + pets[i] + ","); };
+
 ///// Question 2
 // Change my friend's hair color to "green", without modifying the line below.
 
 var friend = { name: 'Chris', age: 27, hairColor: 'blue' };
 
+friend.hairColor = 'green';
+//console.log(friend.hairColor);
+
 ///// Question 3
 // The function below is problematic. In 1-2 sentences explain why, and then fix the code.
 
 var squarePlusOne = function(n){
-  squared = n * n;
+  var squared = n * n;
   return squared + 1;
 };
+
+console.log(squarePlusOne(5));
+//squared declared without a var makes it a global variable.
 
 ///// Question 4
 // Translate the Ruby method defined below into a JavaScript function.
@@ -31,6 +39,17 @@ def greet(name, excited)
 end
 */
 
+var greet = function(name,excited){
+  if(excited) {
+    return "Hi, " + name.toUpperCase() + " here!";
+  } else {
+    return "Hi, " + name + " here!";
+  }
+}
+
+console.log(greet("Jenny",true));
+console.log(greet("Marcy",false));
+
 ///// Question 5
 // Consider the following code.
 
@@ -41,16 +60,18 @@ var anna = {name: "Anna",
             }
           }
 
-// What is `anna`? (e.g. method, function, string, etc.) 
+// What is `anna`? (e.g. method, function, string, etc.)
+//an object literal
 
+// What are `name`, `age`, and `luckyNumber`?
+// attributes
 
-// What are `name`, `age`, and `luckyNumber`? 
+// What does `this` refer to?
+//It's a pointer and points to the object for which the method is called.
 
+// If you want to return the value of anna's lucky number, how would you do that?
+console.log(anna.luckyNumber());
 
-// What does `this` refer to? 
-
-
-// If you want to return the value of anna's lucky number, how would you do that? 
-
-
-// If you want to change anna's name to "wurble", how would you do that? 
+// If you want to change anna's name to "wurble", how would you do that?
+anna.name = "Wurble";
+console.log(anna.name);
